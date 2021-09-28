ba82dca9c4c393ab54998e5af8b5055aa5679fc8
TheHobnobler <53322601+TheHobnobler@users.noreply.github.com>
2021-09-27 22:07:36 -0400

    Update helloworld.php

--- helloworld.php
+++ helloworld.php
@@ -11,3 +11,23 @@
 </body>
 </html>
 
+<?php
+
+
+    $servername = "127.0.0.1:3306";
+    $username = "root";
+    $password = "";
+    $db = "timestamp_db";// Create connection
+    $conn = new mysqli($servername, $username, $password, $db);
+
+
+    //Create connection
+    $conn = new;
+    $mysqli($servername, $username, $password, $db);
+
+    if ($mysqli->ping()) {
+  printf ("Our connection is ok!\n"); 
+} else {
+  printf ("Error: %s\n", $mysqli->error); 
+}
+?>
