652b69077f6b352ca60322662d4d1360ac5401d2
TheHobnobler <53322601+TheHobnobler@users.noreply.github.com>
2021-09-27 22:13:04 -0400

    Update helloworld.php

--- helloworld.php
+++ helloworld.php
@@ -6,6 +6,16 @@
  <?php echo '<p>Hello World</p>'; ?>
 
 
+<?php
+      
+        if(isset($_POST['button1'])) {
+            echo "This is Button1 that is selected";
+        }
+        if(isset($_POST['button2'])) {
+            echo "This is Button2 that is selected";
+        }
+    ?>  
+  
 <form method="post">
         <input type="submit" name="Log"
                 value="Log"/>
