9f2b9b02c934ccfeb043e141f0fbf92f4be87d59
CaylebWojnar <91028110+CaylebWojnar@users.noreply.github.com>
2021-09-28 22:19:01 -0400

    Add files via upload

---
+++ ContactUs.php
@@ -0,0 +1,36 @@
+<!DOCTYPE html>
+<html>
+<head>
+<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.1/dist/css/bootstrap.min.css" rel="stylesheet">
+<title>Page Title</title>
+</head>
+<body>
+
+<nav class="navbar navbar-expand-sm bg-dark navbar-dark">
+  <div class="container-fluid">
+    <ul class="navbar-nav">
+      <li class="nav-item">
+        <a class="nav-link active" href="HomePage.php">Home</a>
+      </li>
+     <li class="nav-item">
+        <a class="nav-link" href="index.php">Hello World</a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href="ContactUs.php">Contact Us</a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href="FAQ.php">FAQ</a>
+      </li>
+    </ul>
+  </div>
+</nav>
+
+<div class="container">
+    <h1>Contact Us</h1>
+    <p>Email: coffee@loveyoualatte.com </p>
+    <p>Phone: 614-555-5515</p>
+    <p>Or come visit us at our main location at 123 Named RD, city OH, 12345.</p>
+</div>
+
+</body>
+</html>
---
+++ FAQ.php
@@ -0,0 +1,36 @@
+<!DOCTYPE html>
+<html>
+<head>
+<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.1/dist/css/bootstrap.min.css" rel="stylesheet">
+<title>Page Title</title>
+</head>
+<body>
+
+<nav class="navbar navbar-expand-sm bg-dark navbar-dark">
+  <div class="container-fluid">
+    <ul class="navbar-nav">
+      <li class="nav-item">
+        <a class="nav-link active" href="HomePage.php">Home</a>
+      </li>
+     <li class="nav-item">
+        <a class="nav-link" href="index.php">Hello World</a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href="ContactUs.php">Contact Us</a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href="FAQ.php">FAQ</a>
+      </li>
+    </ul>
+  </div>
+</nav>
+
+<div class="container">
+    <h1>Frequently Asked Questions</h1>
+    <p>How do I order?&emsp;We are working on making ordering available online, hold tight!</p>
+    <p>I can't find the location?&emsp;No worries we are a bit hard to find. We are located in your local mall area next to the fountain!</p>
+    <p>Also feel free to reach out towards our customer support email for any additional questions at coffee@loveyoualatte.com</p>
+</div>
+
+</body>
+</html>
