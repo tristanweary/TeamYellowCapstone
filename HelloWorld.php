ec4ced6547e32fd6472e8aab14a0786a8272e6ef
CaylebWojnar <91028110+CaylebWojnar@users.noreply.github.com>
2021-10-01 10:22:18 -0400

    Update HelloWorld.php

--- HelloWorld.php
+++ HelloWorld.php
@@ -1,9 +1,38 @@
-<h3 style="color: red;">Hello World from</h3>
-<h1 style="background-color: yellow;">Team Yellow!</h1>
-<form id="frm" method="post"  action="?action" >
-    <input type="submit" value="Submit" id="submit" />
-    </form>
+<!DOCTYPE html>
+<html>
+<head>
+<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.1/dist/css/bootstrap.min.css" rel="stylesheet">
+<title>Index</title>
+</head>
+<body>
 
+<nav class="navbar navbar-expand-sm bg-dark navbar-dark">
+  <div class="container-fluid">
+    <ul class="navbar-nav">
+      <li class="nav-item">
+        <a class="nav-link active" href="HomePage.php">Home</a>
+      </li>
+     <li class="nav-item">
+        <a class="nav-link" href="index.php">Hello World</a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href="ContactUs.php">Contact Us</a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href="FAQ.php">FAQ</a>
+      </li>
+    </ul>
+  </div>
+</nav>
+
+<div class="container pt-5">
+    <h3 style="color: red;">Hello World from</h3>
+    <h1 style="background-color: yellow;">Team Yellow!</h1>
+
+    <form id="frm" method="post"  action="?action" >
+        <input type="submit" value="Submit" id="submit" />
+    </form>
+</div>
 
 <?php
 
@@ -19,7 +48,6 @@ $dbname = "timestamp_db";
 $conn = mysqli_connect($servername, $username, $password, $dbname);
 $sql = "INSERT INTO timestamp_tbl() VALUES()";
 /* if  (!$conn) { die("Connect failed: ".mysqli_connect_error()); }
-
 echo "Connect success"; */
 
 if (mysqli_query($conn, $sql)) {
@@ -33,3 +61,5 @@ if (mysqli_query($conn, $sql)) {
 mysqli_close($conn);
 }
 ?>
+</body>
+</html>
