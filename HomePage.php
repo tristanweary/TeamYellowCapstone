8db9b42173c152625002bb154e56ed1662f35bca
CaylebWojnar <91028110+CaylebWojnar@users.noreply.github.com>
2021-10-01 10:26:47 -0400

    Add files via upload

---
+++ HomePage.php
@@ -0,0 +1,36 @@
+<!DOCTYPE html>
+<html>
+<head>
+<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.1/dist/css/bootstrap.min.css" rel="stylesheet">
+<title>Love You a Latte</title>
+</head>
+<body>
+
+<nav class="navbar navbar-expand-sm bg-dark navbar-dark">
+  <div class="container-fluid">
+    <ul class="navbar-nav">
+      <li class="nav-item">
+        <a class="nav-link active" href="HomePage.php">Home</a>
+      </li>
+     <li class="nav-item">
+        <a class="nav-link" href="index.php">Hello World</a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href="ContactUs.php">Contact Us</a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href="FAQ.php">FAQ</a>
+      </li>
+    </ul>
+  </div>
+</nav>
+
+<div class="container">
+    <h1>Welcome to Love You a Latte</h1>
+    <p>This is a work in progress for a home landing page.</p>
+</div>
+
+</body>
+</html>
+
+
