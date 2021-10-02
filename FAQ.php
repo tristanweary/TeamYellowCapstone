93390837ab6d740e19c5e3da53d9fddc6413a4e6
VadimZhura <90975523+VadimZhura@users.noreply.github.com>
2021-10-02 11:13:00 -0400

    Update FAQ.php
    I have added our story to the faq site

--- FAQ.php
+++ FAQ.php
@@ -26,7 +26,20 @@
 </nav>
 
 <div class="container">
-    <h1>Frequently Asked Questions</h1>
+  
+  <h1>Our Story</h1>
+  <p>Love You A Latte</p>
+  <p>We are the oldest coffee house in the city of Columbus State. We started in Leipzig, Germany.<br>
+    Our first cup was poured in Europe in 1711 and it has not stopped since.<br>
+    The NEICE of the owner came to America in 1816. she settled in Ohio and opened up Love you a latte.</p>
+
+<p>We use the original recipes from the original coffee house in Germany with a twist of brews that are popular today.<br>
+  There is no better tasting coffee than our brew. nobody can say that they started their first brew in 1711.<br> 
+  Our coffee is imported from Germany. you cannot have this unique and wonderful tasting coffee any elsewhere in the U.S. It is pure love with a little latte.
+    </p>
+  
+  
+  <h2>Frequently Asked Questions</h2>
     <p>How do I order?&emsp;We are working on making ordering available online, hold tight!</p>
     <p>I can't find the location?&emsp;No worries we are a bit hard to find. We are located in your local mall area next to the fountain!</p>
     <p>Also feel free to reach out towards our customer support email for any additional questions at coffee@loveyoualatte.com</p>
