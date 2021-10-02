ed4d7041e972f08de9d84b1679b6986505e53b81
VadimZhura <90975523+VadimZhura@users.noreply.github.com>
2021-10-02 10:33:41 -0400

    Update HomePage.php
    Added a footer code

--- HomePage.php
+++ HomePage.php
@@ -31,6 +31,18 @@
 </div>
 
 </body>
+      <footer>
+        <style>
+            p2 {background-color: azure;
+                position: fixed;
+                width: 100%;
+                bottom: 0;
+                
+            }
+        </style>
+        <p2>Contact us!<br>
+            <a href="coffee@loveyoualatte.com">yo email me here!</a></p2>
+    </footer>
 </html>
 
 
