b803b3fa57ad4bbde6607c4105fd3ba7de07f8a3
CaylebWojnar <91028110+CaylebWojnar@users.noreply.github.com>
2021-10-02 14:36:28 -0400

    Update ContactUs.php

--- ContactUs.php
+++ ContactUs.php
@@ -2,7 +2,7 @@
 <html>
 <head>
 <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.1/dist/css/bootstrap.min.css" rel="stylesheet">
-<title>Page Title</title>
+<title>Contact Us</title>
 </head>
 <body>
 
